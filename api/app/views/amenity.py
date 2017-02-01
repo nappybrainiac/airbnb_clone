600a7fa26f1caaf27a98891ac862a31e1bf2b2e7
Gloria Bwandungi <30@holbertonschool.com>
2017-02-01 13:20:02 -0500

    Error handling handled

--- api/app/views/amenity.py
+++ api/app/views/amenity.py
@@ -1,35 +1,62 @@
 import flask
 from app import app
 from app.models.amenity import Amenity
-from app.models.base import db
+from app.models.place_amenity import PlaceAmenities
 from flask import request, jsonify, json
 from flask_json import json_response
 from peewee import *
 
 
-'''To return a list of all users as a JSON object'''
+'''To return a list of all amenities as a JSON object'''
 @app.route('/amenities', methods=['GET', 'POST'])
 def amenity_create():
     if request.method == 'GET':
         amenity_list = Amenity.select()
-        order_values = [i.to_hash() for i in amenity_list]
-        return jsonify(order_values)
+
+        '''If there are no amenities, return an error message.'''
+        if amenity_list:
+            order_values = [i.to_hash() for i in amenity_list]
+            return jsonify(order_values)
+
+        else:
+            return jsonify(msg="No records to display"), 404
+
 
     elif request.method == 'POST':
-        amenity_info = Amenity.create(
-            name = request.form['name']
-        )
-        return jsonify(amenity_info.to_hash())
+        '''If the amenity name already exists, return an error message.'''
+        amenity_check = Amenity.select().where(Amenity.name == request.form['name']).first()
 
+        if amenity_check:
+            return jsonify(code=10003, msg="Name already exists"), 409
+
+        else:
+            amenity_info = Amenity.create(
+                name = request.form['name']
+            )
+            return jsonify(amenity_info.to_hash())
+
+'''Return a jsonified list of all attributes of a specified amenity using
+   its id'''
 @app.route('/amenities/<amenity_id>', methods=['GET', 'DELETE'])
 def modify_amenity(amenity_id):
-  amenity = Amenity.get(Amenity.id == amenity_id)
+    amenity = Amenity.get(Amenity.id == amenity_id)
 
-  if request.method == "GET":
-      return jsonify(amenity.to_hash())
+    if request.method == "GET":
+        if amenity:
+            return jsonify(amenity.to_hash())
+        else:
+            return jsonify(msg="This id does not exist."), 404
 
-  elif request.method == 'DELETE':
+    elif request.method == 'DELETE':
       amenity_info = Amenity.select().where(Amenity.id == amenity_id).first()
       amenity_info.delete_instance()
       amenity_info.save()
       return jsonify(msg="Amenity deleted")
+
+'''Return a jsonified list of all attributes of amenities listed in a place
+   using the place_id'''
+
+@app.route('/places/<place_id>/amenities', methods=['GET'])
+def place_amenity(place_id):
+    amenity_place = PlaceAmenities.get(PlaceAmenities.place == place_id)
+    return jsonify(amenity_info.to_hash())
--- api/app/views/city.py
+++ api/app/views/city.py
@@ -11,19 +11,20 @@ import flask
 def get_post_cities(state_id):
     if request.method == 'GET':
         city_list = City.select().where(City.state == state_id)
-        order_values = [i.to_hash() for i in city_list]
-        return jsonify(order_values)
+
+        if city_list:
+            order_values = [i.to_hash() for i in city_list]
+            return jsonify(order_values)
+
+        else:
+            return jsonify(msg="No records to display."), 404
 
     elif request.method == 'POST':
 
         check_city_name = City.select().where(City.name == request.form['name']).where(City.state == state_id)
-
+        '''Check to make sure the city being added to the state is not a duplicate city'''
         if check_city_name:
-            error_msg = {
-                'code': 10002,
-                'msg': 'City already exists in this state'
-            }
-            return jsonify(error_msg), 409
+            return jsonify(code=10002, msg="City already exists in this state."), 409
 
         else:
             city = City.create(
@@ -36,12 +37,19 @@ def get_post_cities(state_id):
 def view_modify_cities(state_id, city_id):
     if request.method == 'GET':
         city_list = City.select().where(City.id == city_id).where(City.state == state_id)
-        order_values = [i.to_hash() for i in city_list]
-        return jsonify(order_values)
+
+        if city_list:
+            order_values = [i.to_hash() for i in city_list]
+            return jsonify(order_values)
+
+        else:
+            jsonify(msg="This city does not exist in this state."), 404
+
+
 
     elif request.method == 'DELETE':
         city_info = City.select().where(City.id == city_id).where(City.state == state_id).first()
         city_info.delete_instance()
         city_info.save()
-        delete_msg = {'msg':'City has been deleted'}
-        return jsonify(delete_msg)
+
+        return jsonify(msg="City has been deleted.")
