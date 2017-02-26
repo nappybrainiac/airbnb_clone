aeb8483b9b3bc1001fac747f86e83c6a599c1f88
Gloria Bwandungi <30@holbertonschool.com>
2017-02-26 09:59:46 -0500

    200 Test & OK Test pass

--- api/tests/index.py
+++ api/tests/index.py
@@ -23,8 +23,36 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_200(self):
           '''Is the HTTP response code equal == 200?'''
-          rv = self.app.get('/')
-          self.assertEqual(rv.status_code, 200)
+          resp = self.app.get('/')
+          self.assertEqual(resp.status_code, 200)
+
+    def test_status(self):
+        '''Is the JSON reponse status == OK?'''
+        resp = self.app.get('/')
+        resp_data = json.loads(resp.data)
+        self.assertEqual(resp_data.get("status"), "OK")
+
+    def test_time(self):
+        '''Is the JSON reponse time == time right now?'''
+        resp = self.app.get('/')
+        now = datetime.now().strftime("%Y/%m/%d %H:%M")
+        try:
+            right_now = datetime.strptime(resp.json['time'], "%Y/%m/%d %H:%M:%S")
+        except ValueError as err:
+            print "The HTTP response 'time' format is incorrect."
+        self.assertEqual(now, right_now.strftime("%Y/%m/%d %H:%M"))
+
+    def test_time_utc(self):
+        '''Is the JSON reponse time == time right now?'''
+        resp = self.app.get('/')
+        now = datetime.now().strftime("%Y/%m/%d %H:%M")
+        try:
+            right_now = datetime.strptime(resp.json['time'], "%Y/%m/%d %H:%M:%S")
+        except ValueError as err:
+          print "The HTTP response 'time' format is incorrect."
+        self.assertEqual(now, right_now.strftime("%Y/%m/%d %H:%M"))
+
+
 
 
 if __name__ == '__main__':
