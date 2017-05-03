defb7ee4a9373ccc8c1e8a8a9ae919c0565ac049
Gloria Bwandungi <30@holbertonschool.com>
2017-05-03 10:37:45 -0400

    removed error handling for 500

--- api/app/views/index.py
+++ api/app/views/index.py
@@ -4,37 +4,35 @@ from app.models.base import *
 from app.models.base import db
 import datetime
 
+
 @app.route('/', methods=['GET'])
 def index():
     return json_response(
-    status="OK",
-    utc_time=datetime.datetime.utcnow(),
-    time=datetime.datetime.now()
+        status="OK",
+        utc_time=datetime.datetime.utcnow().strftime('%Y/%m/%d %H:%M:%S'),
+        time=datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
     )
 
-'''
-Creating a connection to the database
-'''
+
+# Creating a connection to the database
 @app.before_request
 def before_request():
     db.connect()
 
-'''
-Database connections must always be closed
-'''
+
+# Database connections must always be closed
 @app.after_request
 def after_request(response):
     db.close()
     return response
 
-'''
-This function handles 404 errors.
-'''
+
+# This function handles 404 errors.
 @app.errorhandler(404)
 def not_found(error):
     data = {
-    'code':'404',
-    'msg':'not found'
+        'code': '404',
+        'msg': 'not found'
     }
     error = jsonify(data)
     error.status_code = 404
