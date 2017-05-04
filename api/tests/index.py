2940650f3e8e6378d7881305c7cdf137a255a44a
Gloria Bwandungi <30@holbertonschool.com>
2017-05-04 18:28:23 -0400

    200, status, time, utc tests complete

--- api/tests/index.py
+++ api/tests/index.py
@@ -13,14 +13,28 @@ class AppTestCase(unittest.TestCase):
         self.client = self.app.test_client()
 
     def test_200(self):
-        test_res = self.client.get('/')
-        assert test_res.status_code == 200, \
-            "Status Code is NOT 200, or does not exist"
+        res = self.client.get('/')
+        assert res.status_code == 200, \
+            "Status Code is NOT 200"
 
     def test_status(self):
-        test_res = self.client.get('/')
-        assert test_res.json['status'] == "OK", \
-            "Status is NOT OK, or does not exist"
+        res = self.client.get('/')
+        assert res.json['status'] == "OK", \
+            "Status is NOT OK"
+
+    def test_time(self):
+        res = self.client.get('/')
+        right_now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        # changing time_dict from json to dictionary
+        time_dict = json.loads(res.data)
+        self.assertEqual(time_dict['time'], right_now)
+
+    def test_time_utc(self):
+        res = self.client.get('/')
+        utc_time_now = datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")
+        # changing utc_time_now from json to dictionary
+        utc_dict = json.loads(res.data)
+        self.assertEqual(utc_dict['utc_time'], utc_time_now)
 
 
 if __name__ == '__main__':
